a79a3b3d229a07fc38ca240b6183f790f30277ad
aleks <aleksander.australia@gmail.com>
2020-10-07 02:58:57 +1100

    aassocs with langs and translations

--- src/entity/Sheet.ts
+++ src/entity/Sheet.ts
@@ -1,5 +1,15 @@
-import { Column, Entity, ManyToOne } from 'typeorm';
+import {
+	Column,
+	Entity,
+	JoinColumn,
+	JoinTable,
+	ManyToMany,
+	ManyToOne,
+	OneToOne,
+} from 'typeorm';
+import { Language } from './Language';
 import { Meta } from './Meta';
+import { TranslationGroup } from './TranslationGroup';
 import { User } from './User';
 
 @Entity()
@@ -18,4 +28,16 @@ export class Sheet extends Meta {
 
 	@ManyToOne(() => User, (user) => user.forkedSheets, { nullable: true })
 	forkedFrom: User;
+
+	@OneToOne(() => Language)
+	@JoinColumn()
+	from: Language;
+
+	@OneToOne(() => Language)
+	@JoinColumn()
+	to: Language;
+
+	@ManyToMany(() => TranslationGroup)
+	@JoinTable()
+	translationGroups: TranslationGroup[];
 }
