47c0b4d1b450cf9f9ec08c534099075ef1c18056
aleks <aleksander.australia@gmail.com>
2020-10-07 03:27:36 +1100

    aditional details in user

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,11 +1,20 @@
 import { Column, Entity, OneToMany } from 'typeorm';
-import { Meta } from './Meta';
+import { Meta } from './Shared';
 import { Sheet } from './Sheet';
 
 @Entity()
 export class User extends Meta {
 	@Column()
 	name: string;
+
+	@Column()
+	email: string;
+
+	@Column({ default: false })
+	verified: boolean;
+
+	@Column({ default: false })
+	admin: boolean;
 
 	@OneToMany(() => Sheet, (sheet) => sheet.createdBy)
 	sheets: Sheet[];
